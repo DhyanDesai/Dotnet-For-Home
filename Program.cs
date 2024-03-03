78f98b94d601bcaf7af6cc867331ea848d03fcdb
Dhyandesai <desaidhyan07@gmail.com>
2024-03-03 21:49:03 +0530

    some tasks

--- Program.cs
+++ Program.cs
@@ -1,9 +1,16 @@
-﻿namespace MyDotnet;
+﻿using Name;
 
-class Program
+namespace MyDotnet
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello, World!");
+        static void Main(string[] args)
+        {
+            // Arraylist al2 = new Arraylist();
+            // al2.arraylistex();
+
+            Hashtableex htex1 = new Hashtableex();
+            htex1.htex();
+        }
     }
 }
